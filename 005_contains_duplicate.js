03ec28927f4db0eaf970a10e509c81bb6907eb2c
Shivaji Thattikota <shivajithattikota0410@gmail.com>
2025-10-31 10:14:58 +0530

    Contains duplicate

---
+++ 005_contains_duplicate.js
@@ -0,0 +1,26 @@
+// Problem: https://leetcode.com/problems/contains-duplicate-ii/description
+
+const containsDuplicate = (nums, k) => {
+
+    let mySet = new Set(nums);
+    if (mySet.size === nums.length) {
+        return false;
+    }
+
+    const myMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (myMap.has(nums[i])) {
+            if ((Math.abs(myMap.get(nums[i]) - i)) <= k) {
+                return true;
+            }
+        }
+        myMap.set(nums[i], i);
+    }
+    return false;
+
+}
+
+// Testcase
+const nums = [1,2,3,1];
+const k = 3;
+console.log(containsDuplicate(nums,k))
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Solve leetcode questions daily
 | 1       | 28/10/2025 | Two Sum                         |
 | 2       | 28/10/2025 | Best Time to Buy and Sell Stock |
 | 3       | 29/10/2025 | Maximum Subarray Sum            |
-| 4 | 30/10/2025 | Maximum Average Subarray |
+| 4       | 30/10/2025 | Maximum Average Subarray        |
+| 5       | 31/10/2025 | Contains Duplicate              |
